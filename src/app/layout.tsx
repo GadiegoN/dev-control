a8a57eb7963a785638ba7dca76752867989351ac
GadiegoN <ngadiego@gmail.com>
2024-10-17 14:02:49 -0300

    chore: add login and logout using prisma and nextauth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Header } from "@/components/header";
+import { AuthProvider } from "@/providers/auth";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,8 +30,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header />
-        <main className="w-11/12 max-w-7xl mx-auto py-4">{children}</main>
+        <AuthProvider>
+          <Header />
+          <main className="w-11/12 max-w-7xl mx-auto py-4">{children}</main>
+        </AuthProvider>
       </body>
     </html>
   );
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,20 @@
+"use client";
+
 import Link from "next/link";
-import { FiLogOut, FiUser } from "react-icons/fi";
+import { FiLoader, FiLock, FiLogOut, FiUser } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export function Header() {
+  const { status, data } = useSession();
+
+  async function handleLogin() {
+    await signIn();
+  }
+
+  async function handleLogOut() {
+    await signOut();
+  }
+
   return (
     <header className="h-20 w-full flex items-center shadow-2xl">
       <div className="w-11/12 max-w-7xl mx-auto flex items-center justify-between">
@@ -11,14 +24,28 @@ export function Header() {
           </h1>
         </Link>
 
-        <div className="flex gap-4">
-          <Link href="/dashboard">
-            <FiUser size={26} />
-          </Link>
+        {status === "loading" && (
           <button>
-            <FiLogOut size={26} />
+            <FiLoader className="animate-spin" size={26} />
           </button>
-        </div>
+        )}
+
+        {status === "unauthenticated" && (
+          <button onClick={handleLogin}>
+            <FiLock size={26} />
+          </button>
+        )}
+
+        {status === "authenticated" && (
+          <div className="flex gap-4">
+            <Link href="/dashboard">
+              <FiUser size={26} />
+            </Link>
+            <button onClick={handleLogOut}>
+              <FiLogOut size={26} />
+            </button>
+          </div>
+        )}
       </div>
     </header>
   );
